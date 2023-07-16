4059b363cb82eccf8e9ae33f08eb574eabd64628
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2023-07-16 23:24:17 +0530

    Updates - Separated Pages

--- tedx_script.js
+++ tedx_script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     navSection(document.querySelector('#nav-section'))
-    makePanels(document.querySelector('#team-section'))
-
-    mapSize()
+    if(document.querySelector('#team-section')){makePanels(document.querySelector('#team-section'))}
+    makeFooter()
 })
 
-window.onresize(()=>{
-    mapSize()
-})
 
 function mapSize() {
     const map = document.querySelector("#footer-map")
@@ -39,14 +35,15 @@ function makePanels(section) {
     section.append(table)
 }
 
+
 function navSection(nav_section) {
     var nav_bar  = document.createElement('ul')
     nav_bar.id = "nav-bar"
     nav_section.appendChild(nav_bar) 
     var navPages = {
-        "Home" : "#main-div",
-        "About" : "#about-section",
-        "Team":"#team-section",
+        "Home" : "index.html",
+        "About" : "index.html#about-section",
+        "Team":"teamPage.html",
         "Register":"https://forms.google.com",
         "Contact" : "#footer-table"
     }
@@ -62,4 +59,41 @@ function navSection(nav_section) {
         nav_bar.appendChild(nav_page) 
     }
 
+}
+
+function makeFooter() {
+    const footer = document.querySelector('footer')
+    footer.innerHTML = `
+    <div id="footer-div">
+                <table id="footer-table">
+                    <tr>
+                        <td>
+                            <h2>What</h2>
+                            <h4>TEDx</h4><br><p>OakridgeBachupally</p>
+                        </td>
+                        <td>
+                            <h2>When</h2>
+                            <p>August 24, 2023</p>
+                        </td>
+                        <td>
+                            <h2>
+                                Where
+                            </h2>
+                            <p>Oakridge International School, 
+                                Bachupally, <br>
+                                Hyderabad, Telangana</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div id="map-div">
+                <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3803.8374696636856!2d78.3824723740033!3d17.562923197726835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb8dc64ffffff7%3A0x8156e1ed344f53bb!2sOakridge%20International%20School%2C%20Bachupally!5e0!3m2!1sen!2sin!4v1689479630815!5m2!1sen!2sin"  
+                height="${window.innerHeight * 0.35}"
+                width="${window.innerWidth -10}"
+                id="footer-map"
+                title="location"
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div> 
+    `
 }
