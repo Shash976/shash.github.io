24140ef0e1d82b5e6dbef1b7df8b44e751792c6f
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2023-07-20 11:05:52 +0530

    added google form link

--- tedx_script.js
+++ tedx_script.js
@@ -70,7 +70,7 @@ function navSection(nav_section) {
         "About" : "about",
         "Speakers":"speakers",
         "Team":"team",
-        "Register":"https://forms.google.com",
+        "Register":"https://docs.google.com/forms/d/1Pq5ODSONzMcON7igHOM9j-mc4M2MqUAf6c5ek-QjHlg/edit",
         "Contact" : "contact"
     }
 
